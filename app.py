db90922e9cbfda3cea82a1233cff8b7c70e87daa
MusaTamzid05 <tamzid101@gmail.com>
2020-03-09 19:29:23 +0600

    trackbar added.

--- app.py
+++ app.py
@@ -1,10 +1,113 @@
 import cv2
+from processor import Processor
+
+lower_hue = 10
+higher_hue = 100
+
+lower_segmentation = 10
+higher_segmentation = 100
+
+
+lower_value = 10
+higher_value = 100
+
+def show_values():
+
+    print("Lower Hue : {}".format(lower_hue))
+    print("Higher Hue : {}".format(higher_hue))
+    print("Lower Segmentation : {}".format(lower_segmentation))
+    print("Higher Segmentation : {}".format(higher_segmentation))
+    print("Lower Value : {}".format(lower_value))
+    print("Higher AValue : {}".format(higher_value))
+
+def update_lower_hue(value):
+
+    global lower_hue
+
+    if value > higher_hue:
+        print("cannot update => lower_h > higher_h")
+        return
+
+    lower_hue = value
+    show_values()
+
+
+def update_higher_hue(value):
+
+    global higher_hue
+
+    if value < lower_hue:
+        print("Cannot update => lower_hue > higher_hue")
+        return
+
+    higher_hue = value
+    show_values()
+
+
+def update_lower_segmentation(value):
+
+    global lower_segmentation
+
+    if value >  higher_segmentation:
+        print("cannot update => lower_segmentation > higher_segmentation")
+        return
+
+    lower_segmentation = value
+    show_values()
+
+def update_higher_segmentation(value):
+
+    global higher_segmentation
+
+    if value < lower_segmentation:
+        print("cannot update => lower_segmentation > higher_segmentation")
+        return
+
+    higher_segmentation = value
+    show_values()
+
+
+def update_lower_value(value):
+
+    global lower_value
+
+    if value > higher_value:
+        print("cannot update => lower_value > higher_value")
+        return
+
+    lower_value = value
+    show_values()
+
+
+def update_higher_value(value):
+
+    global higher_value
+
+    if value < lower_value:
+        print("cannot update => lower_value > higher_segmentation")
+        return
+
+    higher_value = value
+    show_values()
+
+
 
 class App:
 
     def __init__(self , src):
         self.src = src
-        self.window_name =  "Press q to quit"
+        self._init_window()
+        self.processor = Processor()
+
+    def _init_window(self):
+        self.window_name =  "Window"
+        cv2.namedWindow(self.window_name , cv2.WINDOW_NORMAL)
+        cv2.createTrackbar("lower_h" , self.window_name , lower_hue , 180 ,  update_lower_hue)
+        cv2.createTrackbar("higher_h" , self.window_name , higher_hue , 180 ,  update_higher_hue)
+        cv2.createTrackbar("lower_s" , self.window_name , lower_segmentation , 180 ,  update_lower_segmentation)
+        cv2.createTrackbar("higher_s" , self.window_name , higher_segmentation , 180 ,  update_higher_segmentation)
+        cv2.createTrackbar("lower_v" , self.window_name , lower_value, 180 ,  update_lower_value)
+        cv2.createTrackbar("higher_v" , self.window_name , higher_value , 180 ,  update_higher_value)
 
     def run(self):
 
@@ -30,10 +133,6 @@ class App:
         cap.release()
         cv2.destroyAllWindows()
 
-
-
-
-
     def _display_image(self):
 
         image = cv2.imread(self.src)
@@ -42,21 +141,14 @@ class App:
             print("No a valid image => {}".format(self.src))
             return
 
+        image = self.processor.process(image)
         self._show(image)
         cv2.waitKey()
 
     def _show(self , image):
 
-        cv2.namedWindow(self.window_name , cv2.WINDOW_NORMAL)
         cv2.imshow(self.window_name , image)
 
 
 
 
-def main():
-
-    app = App(src = "./test.mp4")
-    app.run()
-
-if __name__ == "__main__":
-    main()
---
+++ processor.py
@@ -0,0 +1,9 @@
+
+class Processor:
+
+    def __init__(self):
+        pass
+
+    def process(self , image):
+        return image
+
